48a8241a4b9ef15de12b516af3683919c47b6a2c
Tien Do Thanh <dothanhtien95@gmail.com>
2022-05-14 23:03:12 +0700

    [swagger] add Movie schema

--- src/docs/components/index.js
+++ src/docs/components/index.js
@@ -1,9 +1,11 @@
 const securitySchemes = require("./securitySchemes");
 const User = require("./user");
+const Movie = require("./movie");
 module.exports = {
   components: {
     schemas: {
       ...User,
+      ...Movie,
     },
     securitySchemes,
   },
