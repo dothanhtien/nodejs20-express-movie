947f44afeb8f8cbe49b521e5a9a7231548512560
Tien Do Thanh <dothanhtien95@gmail.com>
2022-05-23 18:11:47 +0700

    document signUp api

--- src/docs/paths/auth/index.js
+++ src/docs/paths/auth/index.js
@@ -1,57 +1,7 @@
+const signIn = require("./signIn");
+const signUp = require("./signUp");
+
 module.exports = {
-  "/auth/sign-in": {
-    post: {
-      tags: ["Auth"],
-      requestBody: {
-        required: true,
-        content: {
-          "application/json": {
-            schema: {
-              type: "object",
-              properties: {
-                email: {
-                  type: "string",
-                  example: "user@example.com",
-                },
-                password: {
-                  type: "string",
-                  example: "11111111",
-                },
-              },
-            },
-          },
-        },
-      },
-      responses: {
-        200: {
-          description: "Success",
-          content: {
-            "application/json": {
-              schema: {
-                type: "object",
-                properties: {
-                  status: {
-                    type: "string",
-                    example: "success",
-                  },
-                  data: {
-                    type: "object",
-                    properties: {
-                      user: {
-                        type: "object",
-                        $ref: "#/components/schemas/User",
-                      },
-                      accessToken: {
-                        type: "string",
-                      },
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
-      },
-    },
-  },
+  ...signIn,
+  ...signUp,
 };
