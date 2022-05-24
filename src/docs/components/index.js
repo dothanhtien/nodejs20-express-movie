6c70b225865c428921dd28008b550a2054332da1
Tien Do Thanh <dothanhtien95@gmail.com>
2022-05-24 22:38:28 +0700

    add CinemaComplex and Cinema schemas

--- src/docs/components/index.js
+++ src/docs/components/index.js
@@ -1,11 +1,15 @@
 const securitySchemes = require("./securitySchemes");
 const User = require("./user");
 const Movie = require("./movie");
+const CinemaComplex = require("./cinemaComplex");
+const Cinema = require("./cinema");
 module.exports = {
   components: {
     schemas: {
       ...User,
       ...Movie,
+      ...CinemaComplex,
+      ...Cinema,
     },
     securitySchemes,
   },
