4823966493312ceca352f7b6b3d0418ba8ce93ba
Tien Do Thanh <dothanhtien95@gmail.com>
2022-05-31 16:26:09 +0700

    update api document for movie module

--- src/docs/paths/users/getUsers.js
+++ src/docs/paths/users/getUsers.js
@@ -1,7 +1,7 @@
 module.exports = {
   get: {
     tags: ["Users"],
-    summary: "Return a list of users",
+    summary: "Return a list of users and pagination",
     security: [{ bearerAuth: [] }],
     parameters: [
       {
